99eab18fe9b49cf9e4ea996b85b695b91423e1a9
Kylee <u0844864@utah.edu>
2019-09-27 14:30:42 -0600

    First push

---
+++ Assets/CameraFlipper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFlipper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        //Flip the user 180 degrees.
+        if(Input.GetKeyDown("F"))
+        {
+            transform.rotation = Quaternion.Euler(0, 180.0f, 0);
+        }
+	}
+}
---
+++ Assets/VRMirror.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRMirror : MonoBehaviour {
+
+    //True = mirror
+    //False = matching
+    bool mirroring;
+
+    Vector3 lastPosition;
+    Quaternion lastRotation;
+    public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+
+        lastPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown("M"))
+        {
+            mirroring = !mirroring;
+        }
+
+        if (mirroring)
+            mirror();
+        else
+            match();
+
+        lastPosition = this.transform.position;
+        lastRotation = this.transform.rotation;
+	}
+
+    void mirror()
+    {
+
+        //Position.
+        Vector3 deltaPosition = this.transform.position - lastPosition;
+        Vector3 v = cube.transform.position - this.transform.position;
+        v = v.normalized;
+
+        cube.transform.position =
+            cube.transform.position + deltaPosition - Vector3.Dot(deltaPosition, v) * v * 2;
+
+        //Rotation.
+        Quaternion deltaRotation = this.transform.rotation * Quaternion.Inverse(lastRotation); //-?
+
+    }
+
+    void match()
+    {
+        Vector3 deltaPosition = this.transform.position - lastPosition;
+        Quaternion deltaRotation = this.transform.rotation * Quaternion.Inverse(lastRotation); //-?
+
+        cube.transform.position = cube.transform.position + deltaPosition;
+        cube.transform.rotation = cube.transform.rotation * deltaRotation;
+    }
+}
